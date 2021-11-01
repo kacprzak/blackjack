48f2f713ad84ec8ac004e11ac6cf007fb834f2e6
Marcin Kacprzak <mkacprzak@novomatic-tech.com>
2021-11-01 21:59:11 +0100

    Haskell-language-server suggestions

--- app/Blackjack.hs
+++ app/Blackjack.hs
@@ -1,12 +1,11 @@
+import Cards
+import Control.Monad.State.Lazy
+import Data.Char
+import Data.Either
+import Data.List
 import System.IO
 import System.Random
 import System.Random.Shuffle
-import Control.Monad.State.Lazy
-import Data.List
-import Data.Char
-import Data.Either
-
-import Cards
 
 type Deck = [Card]
 
@@ -24,7 +23,7 @@ data PlayerAction = None | Hit | Stand | DoubleDown | Split | Surrender deriving
 isPlayerFinished :: Player -> Bool
 isPlayerFinished p
   | isLeft bs = True
-  | bs == (Right 21) = True
+  | bs == Right 21 = True
   | la == Stand = True
   | la == DoubleDown = True
   | la == Surrender = True
@@ -33,52 +32,59 @@ isPlayerFinished p
     bs = score $ hand p
     la = lastAction p
 
-data Player = Player {
-  hand :: [Card],
-  lastAction :: PlayerAction
-}
+data Player = Player
+  { hand :: [Card],
+    lastAction :: PlayerAction
+  }
 
 instance Show Player where
   show (Player hand lastAction) =
     showCards hand ++ "\t(" ++ showScore hand ++ ")\t" ++ show lastAction
     where
-      showCards = intercalate " " . map show . reverse
+      showCards = unwords . map show . reverse
       showScore = show . score
 
 hit :: Player -> State Deck Player
 hit p = do
   c <- getCard
-  return $ p { hand = (c:(hand p))
-             , lastAction = Hit }
+  return $
+    p
+      { hand = c : hand p,
+        lastAction = Hit
+      }
 
-data Table = Table {
-  deck :: Deck,
-  player :: Player,
-  casino :: Player
-}
+data Table = Table
+  { deck :: Deck,
+    player :: Player,
+    casino :: Player
+  }
 
 instance Show Table where
   show s =
-    "Casino: " ++ (show $ casino s) ++ "\n" ++
-    "Player: " ++ (show $ player s)
+    "Casino: " ++ show (casino s) ++ "\n"
+      ++ "Player: "
+      ++ show (player s)
 
 newTable :: Deck -> Table
-newTable d = Table { deck = newDeck
-                   , player = Player h1 None
-                   , casino = Player h2 None }
-  where deal = do
-               h1 <- getCards 2
-               h2 <- getCards 2
-               return (h1, h2)
-        ((h1, h2), newDeck) = runState deal d
-
+newTable d =
+  Table
+    { deck = newDeck,
+      player = Player h1 None,
+      casino = Player h2 None
+    }
+  where
+    deal = do
+      h1 <- getCards 2
+      h2 <- getCards 2
+      return (h1, h2)
+    ((h1, h2), newDeck) = runState deal d
 
 -- STATE TRANSITIONS
 
 playerAction :: PlayerAction -> Player -> State Deck Player
 playerAction Hit p = hit p
 playerAction DoubleDown p = hit p >>= playerAction DoubleDown
-playerAction a p = return $ p { lastAction = a }
+playerAction a p = return $ p {lastAction = a}
 
 -- INPUT/OUTPUT
 
@@ -92,24 +98,27 @@ safeRead _ = Nothing
 
 prompt :: IO String
 prompt = putStr "> " >> hFlush stdout >> getLine
-  
+
 playerDecision :: Player -> IO PlayerAction
 playerDecision p = do
   putStrLn "[H]it, [s]tand, [d]ouble down, s[p]lit or s[u]rrender?"
   s <- prompt
-  case (safeRead (map toLower s)) of
+  case safeRead (map toLower s) of
     (Just d) -> return d
     Nothing -> playerDecision p
 
 casinoDecision :: Player -> IO PlayerAction
-casinoDecision p = let cs = scores $ hand p
-                       bs = fromRight 22 $ score $ hand p
-                       action = if (bs < 17) || (bs == 17 && minimum cs < 17)
-                                then Hit else Stand
-                   in return action
+casinoDecision p =
+  let cs = scores $ hand p
+      bs = fromRight 22 $ score $ hand p
+      action =
+        if bs < 17 || bs == 17 && minimum cs < 17
+          then Hit
+          else Stand
+   in return action
 
 playerLoop :: Player -> (Player -> IO PlayerAction) -> Deck -> IO (Player, Deck)
-playerLoop p logic deck = do
+playerLoop p logic deck =
   if isPlayerFinished p
     then return (p, deck)
     else do
@@ -124,13 +133,14 @@ gameLoop table = do
   print table
   (p', d') <- playerLoop (player table) playerDecision (deck table)
   -- TODO: no need for casino loop if player busted
-  (c', _) <- playerLoop (casino table) casinoDecision (d')
+  (c', _) <- playerLoop (casino table) casinoDecision d'
   return $ compareScore (score $ hand p') (score $ hand c')
 
 playGame :: Deck -> IO ()
 playGame d = gameLoop (newTable d) >>= print
 
 main :: IO ()
-main = do g <- getStdGen
-          let d = shuffle' frenchDeck (length frenchDeck) g
-          playGame d
+main = do
+  g <- getStdGen
+  let d = shuffle' frenchDeck (length frenchDeck) g
+  playGame d
